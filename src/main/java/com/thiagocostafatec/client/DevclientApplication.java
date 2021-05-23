17b210e772369d0744abacda4560176f61b77304
Thiago <thiagosilva270292@gmail.com>
2021-05-22 21:45:01 -0300

    Client SpringProject

---
+++ src/main/java/com/thiagocostafatec/client/DevclientApplication.java
@@ -0,0 +1,13 @@
+package com.thiagocostafatec.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevclientApplication.class, args);
+	}
+
+}
